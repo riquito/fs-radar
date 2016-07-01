07bb4fe4a7ddf22477f05fba7ae22054746b4f8b
Riccardo Attilio Galli <riccardo@sideralis.org>
2016-07-01 20:02:33 +0200

    Rewrite path_filter tests in tests/test_path_filter

--- src/path_filter.py
+++ src/path_filter.py
@@ -18,7 +18,7 @@ def ruleToRegexp(rule):
     # (we're interested in the first(s) occurrence(s) )
     rule = re.sub('([^.]|^)(\\./)+', '\\1', rule)
 
-    return ('(.*/)?' if any_depth else '^') + \
+    return ('(.*/)?' if any_depth else '^(\\./)?') + \
         re.escape(
              # substitute multiple asterisks with a single *
              re.sub('\*+', '*', rule)
@@ -47,50 +47,16 @@ def makePathFilter(rules):
             reg = ruleToRegexp(rule)
             includeRegExps.append(reg)
 
-    INCLUDE_REGEXP = re.compile('|'.join(includeRegExps))
-    EXCLUDE_REGEXP = re.compile('|'.join(excludeRegExps))
+    INCLUDE_REGEXP = re.compile('|'.join(includeRegExps)) if includeRegExps else None
+    EXCLUDE_REGEXP = re.compile('|'.join(excludeRegExps)) if excludeRegExps else None
     DO_NOT_EXCLUDE_REGEXP = re.compile('|'.join(doNotExcludeRegExps)) if doNotExcludeRegExps else None
 
     def path_filter(path):
-        return bool(
-            INCLUDE_REGEXP.match(path) and
-            (not EXCLUDE_REGEXP.match(path) or (DO_NOT_EXCLUDE_REGEXP and DO_NOT_EXCLUDE_REGEXP.match(path)))
+        return bool(path and  # noqa
+            (INCLUDE_REGEXP and INCLUDE_REGEXP.match(path)) and
+            ((not (EXCLUDE_REGEXP and EXCLUDE_REGEXP.match(path))) or
+             (DO_NOT_EXCLUDE_REGEXP and DO_NOT_EXCLUDE_REGEXP.match(path))
+            )
         )
 
     return path_filter
-
-
-if __name__ == '__main__':
-    rules = [
-        "*.js",
-        ".jsx",
-        "a/dir/",
-        "a/glob/**/file.png",
-        "./only-at-root",
-        "app/cache/*",
-        "!app/cache/*.txt",
-        "+app/cache/do-not-exclude-me.txt"
-    ]
-
-    path_filter = makePathFilter(rules)
-
-    assert path_filter('root-file.js')
-    assert path_filter('any/depth/file.js')
-    assert path_filter('.jsx')
-    assert path_filter('any/depth/.jsx')
-    assert path_filter('bar.jsx') is False
-    assert path_filter('only-at-root')
-    assert path_filter('any/depth/only-at-root') is False
-    assert path_filter('') is False
-    assert path_filter('a/dir/')
-    assert path_filter('a/dir/file')
-    assert path_filter('a/dir')
-    assert path_filter('a/dirfoo') is False
-    assert path_filter('a/glob/one-level/file.png')
-    assert path_filter('a/glob/one-level/nofile.png') is False
-    assert path_filter('a/glob/multi/level/file.png')
-    assert path_filter('app/cache/include-me')
-    assert path_filter('app/cache/exclude.txt') is False
-    assert path_filter('app/cache/do-not-exclude-me.txt')
-
-    print('All tests passed')
